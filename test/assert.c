03ae27990b08856d838bae4cd452878f38aee059
Zhigang Song <1005411480a@gmail.com>
2017-07-23 05:31:54 +0800

    more assert test

--- test/assert.c
+++ test/assert.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
-#include "../include/assert.h"
-
 int main()
 {
+#define NDEBUG
+#include "../include/assert.h"
+    assert(0 == 0);
+    assert(0 == 1);
+
+#undef NDEBUG
+#include "../include/assert.h"
+    assert(0 == 0);
     assert(0 == 1);
 
     return 0;
